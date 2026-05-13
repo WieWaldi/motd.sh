8d6a3a101fb9d89cdffea40c6e427892bcd84db1
Waldemar Schröer <w@ldemar.de>
2026-05-13 23:02:00 +0200

    Initial setup files.

---
+++ config.mk
@@ -0,0 +1,11 @@
+# motd.sh
+VERSION = 1.0
+
+# If you prefer to install motd.sh into your home directory,
+# uncomment the following line and comment out the next one.
+# PREFIX = ${HOME}/.local
+PREFIX = /usr/local
+
+CONFDIR = ${PREFIX}/etc
+BASEDIR = ${PREFIX}/share/motd.sh
+MODULES = ${BASEDIR}/modules
